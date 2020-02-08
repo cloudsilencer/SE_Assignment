d9cff3911bc774e4e25167320f69e60f9b86401b
Kevin Toh <kevintoh0305@gmail.com>
2020-02-08 12:15:08 +0800

    Added some manager menu

--- SE Assignment/SE Assignment/Program.cs
+++ SE Assignment/SE Assignment/Program.cs
@@ -10,6 +10,9 @@ namespace SE_Assignment
     {
         static void Main(string[] args)
         {
+
+            manageFoodItems();
+
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Login as Customer");
@@ -40,6 +43,25 @@ namespace SE_Assignment
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+
+            // Manager Functions
+            void manageFoodItems()
+            {
+                string functionTitle = "Manage Food Items";
+                List<String> options = new List<string> { "Add Food Item", "Delete Food Item", "Update Food Item", "Exit"};
+                Console.WriteLine($"{functionTitle}\n{String.Concat(Enumerable.Repeat("-", functionTitle.Length))}");
+                for (int i = 0; i < options.Count; i++)
+                {
+                    if(((i + 1) % 4) == 0)
+                        Console.WriteLine($"\n[{(i + 1) % 4}] {options[i]}");
+                    else
+                        Console.WriteLine($"[{(i + 1) % 4}] {options[i]}");
+                }
+                Console.Write("\nSelect an option: ");
+                String selectedOption = Console.ReadLine();
+                Console.WriteLine("You have selected " + selectedOption + ".");
+            }
         }
     }
 }
